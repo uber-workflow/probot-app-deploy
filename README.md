70dcaaa5097cf478b466237b5fdafdf2711bdbbe
Kevin Grandon <kevingrandon@yahoo.com>
2017-11-07 12:41:52 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# probot-app-release
+
+[![build status][build-badge]][build-href]
+[![dependencies status][deps-badge]][deps-href]
+[![npm version][npm-badge]][npm-href]
+
+> a GitHub App built with [probot](https://github.com/probot/probot) that creates NPM deployments through Buildkite
+
+## Setup
+
+```
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+See [docs/deploy.md](docs/deploy.md) if you would like to run your own instance of this app.
+
+[build-badge]: https://travis-ci.org/uber-web/probot-app-buildkite-deploy.svg?branch=master
+[build-href]: https://travis-ci.org/uber-web/probot-app-buildkite-deploy
+[deps-badge]: https://david-dm.org/uber-web/probot-app-buildkite-deploy.svg
+[deps-href]: https://david-dm.org/uber-web/probot-app-buildkite-deploy
+[npm-badge]: https://badge.fury.io/js/probot-app-buildkite-deploy.svg
+[npm-href]: https://www.npmjs.com/package/probot-app-buildkite-deploy
---
+++ index.js
@@ -0,0 +1,48 @@
+/** Copyright (c) 2017 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const exec = require('child_process').exec;
+
+module.exports = robot => {
+  robot.on('release.published', released);
+
+  async function released(context) {
+    const {github} = context;
+    const {release} = context.payload;
+
+    github.repos.createRelease(
+      context.repo({
+        tag_name: release.tag_name,
+        auto_merge: false,
+      }),
+    );
+
+    // Kick off a deployment through buildkite
+    const {login} = release.author;
+    const {name, ssh_url} = context.payload.repository;
+    const curlCommand = `curl \
+      -H "Authorization: Bearer ${process.env.BUILDKITE_TOKEN}" \
+      -X POST "https://api.buildkite.com/v2/organizations/uberopensource/pipelines/npm-deploy/builds" \
+        -d '{
+          "commit": "HEAD",
+          "branch": "master",
+          "message": "Deploy ${name}",
+          "author": {
+            "name": "${login}"
+          },
+          "env": {
+            "PUBLISH_REPO": "${ssh_url}"
+          }
+        }'`;
+
+    exec(curlCommand, error => {
+      if (error !== null) {
+        // eslint-disable-next-line no-console
+        console.warn('exec error: ' + error);
+      }
+    });
+  }
+};
